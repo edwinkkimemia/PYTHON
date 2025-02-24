450b2c7751c31e32965eeb3b5f0c63cdfac521c5
edd <edwinkkimemia@gmail.com>
2025-02-24 23:43:40 +0300

    first commit

---
+++ python.py
@@ -0,0 +1,10 @@
+print("hello world")
+print("This is a new line of code")
+temperature = 30
+
+if temperature > 25:
+    print("It’s a hot day! ☀️")
+else:
+    print("It’s a cool day! ❄️")
+print("Done")
+print("This is the end of the program")
